fe1e00e6f5ebc1e136e4dc5531168809f8aa5338
Obinna Akpulonu <akpulonujoseph@gmail.com>
2025-02-07 00:23:26 +0100

    Update views.py

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-
 from .cart import Cart
-
 from product.models import Product
 
 def add_to_cart(request, product_id):
